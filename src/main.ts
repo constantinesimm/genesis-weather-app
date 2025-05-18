7e57e082d88d6f72c66f982f046acf738dc2942d
Kostiantyn Peschanov <k.peschanov@atlasapps.eu>
2025-05-18 17:11:01 +0300

    add app structure

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Weather Forecast API')
+    .setDescription(
+      'Weather API application that allows users to subscribe to weather updates for their city.',
+    )
+    .setVersion('1.0.0')
+    .addServer('https://weatherapi.app/api')
+    .addTag('weather')
+    .addTag('subscription')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
